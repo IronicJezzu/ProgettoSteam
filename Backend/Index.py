5a4195eb130ef5010dce9730fde3f5d0241b0907
IronicJezzu <paolinobotta04@gmail.com>
2025-10-08 16:18:09 +0200

    Primi passi con FastAPI

--- Backend/Index.py
+++ Backend/Index.py
@@ -1,3 +1,8 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List
 import os
 import functions 
 from dotenv import load_dotenv
@@ -5,17 +10,33 @@ load_dotenv()
 STEAM_API_KEY = os.getenv('STEAM_API_KEY')    
 BASE_URL = "http://api.steampowered.com"
 
-
-
-
 if not STEAM_API_KEY:
-    raise ValueError("Non hai settato la variabile d'ambiente STEAM_API_KEY")
+    raise ValueError("Non hai settato la variabile d'ambiente STEAM_API_KEY")  #Controllo che la chiave API sia stata caricata correttamente
 
+class Player(BaseModel):     #Modello per i dati del giocatore usato da FastAPI
+    profileurl: str
+    personaname: str
+    personastate: int
+    gameextrainfo: str = None
+    gameid: str = None
+    avatar: str
 
+app =  FastAPI()     #Inizializzazione dell'app FastAPI
+origins = [                     #Lista di origini per il CORS
+    "http://localhost:3000"
+]
+
+app.add_middleware(                 #Permette le richieste CORS dal frontend React
+    CORSMiddleware,      
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 STEAM_ID = "76561199096123419"
 
-players = functions.get_player_summaries(STEAM_ID)
+players = functions.get_player_summaries(STEAM_ID)   #Chiamata alla funzione per ottenere i sommari dei giocatori
 
 for p in players:
     print("URLprofilo", p.get("profileurl"))
@@ -30,3 +51,10 @@ for p in players:
         print("Non sta giocando a nulla")
     else:
         print("Sta giocando a ", p.get("gameextrainfo"))
+
+@app.get("/players/", response_model=List[Player])   #Endpoint per ottenere i dati dei giocatori
+async def read_players():
+    return players
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- Backend/functions.py
+++ Backend/functions.py
@@ -6,8 +6,10 @@ load_dotenv()
 
 STEAM_API_KEY = os.getenv('STEAM_API_KEY')    
 BASE_URL = "http://api.steampowered.com"
+if not STEAM_API_KEY:
+    raise ValueError("Non hai settato la variabile d'ambiente STEAM_API_KEY")  #Controllo che la chiave API sia stata caricata correttamente
 
-def get_player_summaries(steamid):
+def get_player_summaries(steamid):                                        #Funzione per ottenere i sommari dei giocatori
     url=f"{BASE_URL}/ISteamUser/GetPlayerSummaries/v2/"
     param={
         "key":STEAM_API_KEY,
